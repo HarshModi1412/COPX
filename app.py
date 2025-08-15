c73aefb50d9acdaa75e30750ef37177b2858ffde
HarshModi1412 <128783540+HarshModi1412@users.noreply.github.com>
2025-08-15 18:00:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from billing import billing_page
 from inventory import inventory_page
 from billing_history import billing_history_page
 from analyst import analyst_page
-from orders import orders_page          # <-- add this import
+from order_management import order_management_page          # <-- add this import
 from db import init_db
 
 st.set_page_config(page_title="Cafe POS & Inventory", layout="wide")
@@ -12,11 +12,11 @@ init_db()
 
 menu = st.sidebar.radio(
     "Navigation",
-    ["Orders", "Billing", "Inventory Management", "Billing History", "Business Analyst"]  # <-- add Orders
+    ["order_management", "Billing", "Inventory Management", "Billing History", "Business Analyst"]  # <-- add Orders
 )
 
-if menu == "Orders":
-    orders_page()
+if menu == "order_management":
+    order_management_page()
 elif menu == "Billing":
     billing_page()
 elif menu == "Inventory Management":
@@ -25,3 +25,4 @@ elif menu == "Billing History":
     billing_history_page()
 elif menu == "Business Analyst":
     analyst_page()
+
